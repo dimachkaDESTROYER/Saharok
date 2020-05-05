a9ab78c0f152c94c19637fd136bd18bd8f6abfb8
Дима <kolosow-dima@yandex.ru>
2020-05-05 19:59:12 +0500

    оля сделала уровень

--- Saharok/Interface/StartMenu.cs
+++ Saharok/Interface/StartMenu.cs
@@ -1,8 +1,13 @@
 ﻿using Saharok.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Saharok
@@ -30,13 +35,18 @@ namespace Saharok
             play.Click += (sender, args) =>
             {
                 var coins = new List<Rectangle>();
-                for (var y = 200; y <= 250; y += 50)
-                    for (var x = 80; x <= 80 + 50 * 10; x += 50)
+                for (var y = 250; y <= 290; y += 40)
+                    for (var x = 250; x <= 80 + 50 * 10; x += 50)
                         coins.Add(new Rectangle(x, y, 25, 25));
                 coins.Add(new Rectangle(1000, 250, 25, 25));
-                var player = new Player(new Rectangle(25, 200, 50, 50), 2);
-                var monster = new Monster[] { new Monster(320, 560, MovingDirection.Right, new Rectangle(380, 500, 50, 50)) };
-                var finish = new Rectangle(80, 200, 100, 100);
+                coins.Add(new Rectangle(60, 30, 25, 25));
+                coins.Add(new Rectangle(95, 30, 25, 25));
+                coins.Add(new Rectangle(960, 165, 25, 25));
+                var player = new Player(new Rectangle(25, 500, 50, 50), 2);
+                var monster = new Monster[] { new Monster(320, 560, MovingDirection.Right, new Rectangle(380, 500, 50, 50)),
+                                              new Monster(320, 740, MovingDirection.Left, new Rectangle(380, 145, 50, 50)),
+                                              new Monster(580, 850, MovingDirection.Right, new Rectangle(480, 25, 50, 50))};
+                var finish = new Rectangle(880, 5, 100, 75);
                 var l = new LevelBuilder(1024, 600, finish).AddPlayer(player).AddMonsters(monster)
                                                    .AddWalls(new Rectangle(0, 550, 160, 50),
                                                              new Rectangle(320, 550, 320, 50),
@@ -44,9 +54,15 @@ namespace Saharok
                                                              new Rectangle(640, 450, 160, 50),
                                                              new Rectangle(880, 400, 80, 50),
                                                              new Rectangle(960, 325, 80, 50),
-                                                             new Rectangle(600, 325, 160, 50),
-                                                             new Rectangle(320, 325, 160, 50),
-                                                             new Rectangle(80, 300, 160, 50))
+                                                             new Rectangle(600, 350, 160, 25),
+                                                             new Rectangle(320, 350, 160, 25),
+                                                             new Rectangle(80, 300, 160, 25),
+                                                             new Rectangle(20, 225, 120, 25),
+                                                             new Rectangle(320, 195, 440, 25),
+                                                             new Rectangle(880, 195, 120, 25),
+                                                             new Rectangle(580, 75, 400, 25),
+                                                             new Rectangle(280, 95, 160, 25),
+                                                             new Rectangle(40, 70, 160, 25))
                                                    .AddCoins(coins.ToArray())
                                                    .AddLava(new Rectangle(160, 550, 160, 50),
                                                              new Rectangle(640, 550, 480, 50));
@@ -55,7 +71,6 @@ namespace Saharok
                 this.Hide();
             };
             Controls.Add(play);
-
             var settings = new Button()
             {
                 Text = "Настройки",
